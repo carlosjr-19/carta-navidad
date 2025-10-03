c85d10aff2f3525612513d27f5639521c7e156d7
carlosjr-19 <carlosvillarroeljr@hotmail.com>
2025-10-03 17:38:08 -0600

    Adición de cartas familiares

--- carta_navidad/services/cards.py
+++ carta_navidad/services/cards.py
@@ -9,58 +9,190 @@ CARTAS_FAMILIARES = {
         "titulo": "Abuelita Elvia",
         "contenido": """Querida Abuelita,
 
-En esta época tan especial del año, quiero tomarme un momento para decirte lo importante y sabia que eres en nuestras vidas. Siempre con un consejo y palabras cargadas de un profundo amor, sus bendiciones, oraciones y bondades han sido un faro de luz para todos nosotros.
+        En esta época tan especial del año, quiero tomarme un momento para decirte lo importante y sabia que eres en nuestras vidas. Siempre con un consejo y palabras cargadas de un profundo amor, sus bendiciones, oraciones y bondades han sido un faro de luz para todos nosotros.
 
-En este año que pude compartir con usted, agradezco por todos sus consejos, enseñanzas y cuidados mientras compartiamos casa. Aquellas tardes de almuerzo donde podiamos conversar y reir, son momentos que atesoro profundamente.
+        En este año que pude compartir con usted, agradezco por todos sus consejos, enseñanzas y cuidados mientras compartiamos casa. Aquellas tardes de almuerzo donde podiamos conversar y reir, son momentos que atesoro profundamente.
 
-Espero que siempre como muchas veces en México le han comentado, siga viendose tan juvial y llena de vida. con sus cabellos blancos que reflejan la sabiduría de sus años y su sonrisa que ilumina cualquier habitación. Sin olvidar sus extravagantes atuendos.
+        Espero que siempre como muchas veces en México le han comentado, siga viendose tan juvial y llena de vida. con sus cabellos blancos que reflejan la sabiduría de sus años y su sonrisa que ilumina cualquier habitación. Sin olvidar sus extravagantes atuendos.
 
-Espero que esta Navidad esté llena de alegría, amor y momentos inolvidables junto a todos nosotros. Que la magia de esta temporada le traiga paz y felicidad, y que el próximo año esté colmado de bendiciones y salud.
+        Espero que esta Navidad esté llena de alegría, amor y momentos inolvidables junto a todos nosotros. Que la magia de esta temporada le traiga paz y felicidad, y que el próximo año esté colmado de bendiciones y salud.
 
-Con todo mi cariño navideño,
-Como siempre me nombre Carlitos Manuel ❤️""",
+        Con todo mi cariño navideño,
+        Como siempre me nombre Carlitos Manuel ❤️""",
         "imagen_fondo": "https://images.unsplash.com/photo-1700668497475-1e0ce91919c5",
         "imagen_carta": "https://i.postimg.cc/zDpX46yx/abuela-elvia.jpg",
         "color_tema": "red"
     },
-    "maria_1995-12-15": {
+    "elizabeth_1946-10-16": {
         "carta_id": "carta2",
-        "destinatario": "María",
-        "fecha_nacimiento": "1995-12-15",
-        "titulo": "Para mi bella María",
-        "contenido": """Mi querida María,
+        "destinatario": "Abuela Elizabeth",
+        "fecha_nacimiento": "1946-10-16",
+        "titulo": "Abuela Elizabeth",
+        "contenido": """Querida Abuela,
 
-¡Qué regalo tan especial es tenerte en nuestras vidas! Tu cumpleaños tan cerquita de la Navidad siempre ha hecho que esta época sea aún más mágica para toda la familia.
+        Quiero comenzar esta carta agrandeciendote por todo el amor y la atención que me has brindado. Desde que era pequeño siempre me has acogido con los brazos abiertos y un corazón lleno de cariño.
 
-Admiro tu fortaleza, tu inteligencia y la manera tan hermosa en que cuidas de todos nosotros. Eres como una estrella que guía nuestro camino, especialmente en estos tiempos tan digitales donde a veces olvidamos lo importante que es estar unidos.
+        Por tantas veces que en vacaciones me abriste las puertas de tu casa, disfrutando de aquellos almuerzos mágicos que solo tu sabias prepararnos a toda la familia. 
 
-En esta Navidad, quiero que sepas que eres una bendición en nuestras vidas y que estamos muy orgullosos de la mujer increíble en la que te has convertido.
+        Recuerdo con especial cariño aquellas tardes en las que compartíamos historias y risas sentados en el porche, creando recuerdos que atesoro profundamente.
 
-Que este nuevo año te traiga todas las alegrías que mereces y que nuestra familia siga siendo tu refugio de amor y paz.
+        Como olvidar cuando estando allá e incluso despues de regresarme a casa me pedias los números de la loteria para jugar, siempre ligando los números ganadores. Esa era mi labor de nieto más preciada.
 
-Con infinito amor,
-Tu familia que te celebra siempre 🎄""",
+        Sin dejar de lado todos aquellos regaños que como buen Zuliano me dabas, por nunca estar quieto y siempre correr por los pasillos despues que habian pasado el lampazo o barrido.
+
+        Agradezco por permitirme jugar en su patio y jardin con mis primos, ya sea colocando piscinas, tumbando cocos, jugando futbol o incluso bolas criollas. Aunque eso dañara el cesped que con tanto esmero cuidaban mi abuelo y tu. 
+
+        En estas epocas navideñas extraño el compartir contigo y el abuelo. Pero se que siempre estaran conmigo en espiritu y en mi corazon.
+
+        Te quiero mucho Abuela Feliz Navidad 🎅""",
         "imagen_fondo": "https://images.unsplash.com/photo-1576028218625-9de3753b47cd",
+        "imagen_carta": "https://i.postimg.cc/zvq667Jt/abuela-elizabeth.jpg",
         "color_tema": "green"
     },
-    "carlos_1988-07-10": {
+    "angel_1966-11-18": {
         "carta_id": "carta3",
-        "destinatario": "Hermano Carlos",
-        "fecha_nacimiento": "1988-07-10",
-        "titulo": "Para nuestro querido Carlos",
-        "contenido": """Querido Carlos,
+        "destinatario": "Tío Angel",
+        "fecha_nacimiento": "1966-11-18",
+        "titulo": "Querido Tío Angel",
+        "contenido": """Querido Tio Angel,
 
-En esta Navidad digital, como tú siempre dices, queremos recordarte lo analógico que es nuestro amor por ti. Tu manera de ver la vida, siempre con esa mezcla perfecta de sabiduría y humor, hace que cada reunión familiar sea especial.
+        En esta Navidad digital, de lo amante y vanguardista que está siempre con la tecnología, quiero recordarle lo analógico que es mi amor por ti. Su manera de ver la vida, siempre con esa mezcla perfecta de sabiduría y humor, hace que cada reunión familiar sea especial. 
 
-Eres el hermano, el hijo, el tío que todos admiramos. Tu capacidad para resolver cualquier problema y tu forma de hacer reír a todos, incluso en los momentos difíciles, es un regalo que valoramos más que cualquier cosa material.
+        Reuniones donde no faltan sus historias llenas de anécdotas y su risa contagiosa que ilumina cualquier habitación.
+
+        Aprecio profundamente sus cuidados y atención que ha tenido conmigo desde que era pequeño, siempre ofrenciendome consejos para cuidarme lo mejor que pueda, aunque usted no siga mucho sus propios consejos, sin embargo como siempre escuche su frase icónica "Haga lo que les digo, no lo que hago" siempre con una sonrisa y un toque de humor que hace que todo parezca más ligero.
+
+        Le doy las gracias por cada momento compartido y aventura vivida. Desde la semana santa que me adopto junto a mis primos como su hijo, las noches estrelladas en la gran sábana, las visita al glorioso Santiago bernabeu y tantas otras experiencias que han enriquecido mi vida.
+
+        Tío Padrino, su presencia en mi vida es un regalo para mi esta navidad. Así que espero que la suya esté llena de alegría, amor y momentos inolvidables junto a todos sus seres queridos.
+
+        Lo amo y extraño, que tengas una feliz navidad 🎄""",
+        "imagen_fondo": "https://images.unsplash.com/photo-1513519683267-4ee6761728ac",
+        "imagen_carta": "https://i.postimg.cc/FswWNsc0/IMG-20240914-WA0109.jpg",
+        "color_tema": "gold"
+    },
+    "genaro_1968-01-17": {
+        "carta_id": "carta4",
+        "destinatario": "Tío Genaro",
+        "fecha_nacimiento": "1968-01-17",
+        "titulo": "Querido Tío Genero",
+        "contenido": """Querido Tío Genaro,
+
+        Siempre lo recuerdo como mi tío mas competitivo, por las veces que jugabamos en su casa basquet, o sino era voleyball pero siempre disfrutando de la compañía del otro.
+
+        Como olvidar tambien en cada evento o fiesta sus icónicos pasos de baile que por más que intentaba igualar no me salian. 
+
+        Son algunas de las cosas que extraño en Navidad, cuando ibamos al Zulia y compartiamos cuentos, anecdotas y viviencias y tambien las creabamos. Las salidas a los parques de agua, ver el atardecer junto al lago de maracaibo cuando volabamos papagayo.
+
+        Son recuerdos que atesoro y agradezco siempre que estuviese allí.
+
+        Le deseo una Feliz navidad y le mando un enorme abrazo 🎄. Lo quiero mucho""",
+        "imagen_fondo": "https://plus.unsplash.com/premium_photo-1700838995227-6647bbf304e6",
+        "imagen_carta": "",
+        "color_tema": "gold"
+    },
+    "david_1972-06-26": {
+        "carta_id": "carta5",
+        "destinatario": "Tío David",
+        "fecha_nacimiento": "1972-06-26",
+        "titulo": "Querido Tío David",
+        "contenido": """Querido Tío David,
+
+        Quisiera comenzar esta carta agradeciendote por todo el amor y cariño que me ha brindado desde que era pequeño siempre me has acogido con los brazos abiertos y un corazón lleno de amor.
+
+        Recordando siempre las navidades zulianas en casa de los abuelos, donde la familia se reunía para celebrar con alegría y compartir momentos inolvidables. Aquellos cenas mágicas llenas de recuerdos y risas.
+
+        El último reencuentro en Madrid, donde me paseo por lugares iconicos de la ciudad, donde a pesar de la distancia, el cariño y la conexión familiar se mantuvieron intactos. Fue un recordatorio de lo importante que es la familia, sin importar dónde estemos en el mundo.
+
+        Por esa razón, en esta Navidad quiero desearte todo lo mejor. Que la magia de esta temporada llene tu hogar de alegría, amor y momentos inolvidables junto a todos tus seres queridos. Que el próximo año esté colmado de bendiciones, salud y felicidad. 
+
+        Lo quiero mucho Tío David, Feliz Navidad 🎅""",
+        "imagen_fondo": "https://plus.unsplash.com/premium_photo-1700838995227-6647bbf304e6",
+        "imagen_carta": "https://i.postimg.cc/6q752BZX/Tio-David.jpg",
+        "color_tema": "gold"
+    },
+    "marco_1974-03-02": {
+        "carta_id": "carta6",
+        "destinatario": "Tío Marco",
+        "fecha_nacimiento": "1974-03-02",
+        "titulo": "Querido Tío Marco",
+        "contenido": """Querido Tío Marco,
+
+        Cada vez que lo recuerdo, me viene a la mente su espíritu de siempre hechar para adelante, no importa la situación. Siempre ha sido un ejemplo de cómo enfrentar los desafíos con una sonrisa y una actitud positiva. Siempre emprendiendo nuevos proyectos y aventuras, demostrando que la vida es para vivirla al máximo.
+
+        Le agradezco por todas las veces que me prestaba su cuarto cada vez que iba a casa de los abuelos, entre la oscuridad y las estrellas del techo parecia que realmente estuviese en el espacio. 
+
+        Recuerdo todos sus adornos y detalles en la casa por estas epocas navideñas, que reflejan su personalidad única y el amor por los detalles.
+
+        Y ahora que pasa una navidad lejos de casa, espero que esté llena de alegría, amor y momentos inolvidables junto a todos sus seres queridos. Que la magia de esta temporada le traiga paz y felicidad, y que el próximo año esté colmado de bendiciones y salud.
+
+        Le mando un especial abrazo Tío Marco, Feliz Navidad, lo quiero mucho 🎅""",
+        "imagen_fondo": "https://plus.unsplash.com/premium_photo-1700838995227-6647bbf304e6",
+        "imagen_carta": "https://i.postimg.cc/tJ1CZ960/Tio-marco-eliecer.jpg",
+        "color_tema": "gold"
+    },
+    "eveliz_1959-08-15": {
+        "carta_id": "carta7",
+        "destinatario": "Tía Eveliz",
+        "fecha_nacimiento": "1959-08-15",
+        "titulo": "Querida Tía Eveliz",
+        "contenido": """Querida Tía Eveliz,
+
+        Desde que era pequeño recuerdo con mucho cariño todas las veces que me acogió en su hogar, en esas visitas esporadicas, ya sea porque ibamos a correr maratones, o simplemente a compartir en familia.
+
+        Los viajes a la playa, los momentos familiares, las risas y las historias que compartimos son momentos que atesoro. Su calidez y generosidad como persona siempre me hicieron sentir feeliz siempre a su lado, y por eso le estoy profundamente agradecido.
+
+        Por eso le deseo una Feliz navidad y le mando un enorme abrazo. La quiero mucho🎄.""",
+        "imagen_fondo": "https://plus.unsplash.com/premium_photo-1700838995227-6647bbf304e6",
+        "imagen_carta": "https://i.postimg.cc/8cFPJTvg/tia-eveliz.jpg",
+        "color_tema": "gold"
+    },
+    "ana_1956-02-19": {
+        "carta_id": "carta8",
+        "destinatario": "Tía Ana",
+        "fecha_nacimiento": "1956-02-19",
+        "titulo": "Querida Tía Ana",
+        "contenido": """Querida Tía Ana,
+
+        Se que es poco lo que hemos compartido a lo largo de los años, pero quiero que sepa que siempre la he admirado por su fortaleza y dedicación a la familia, no ha de ser facil ser la primera familliar que dejaba nuestra tierra, al principio cuando era pequeño no entendía el porqué. Ya luego con el tiempo y las experiencias de la vida, comprendí el gran sacrificio que hizo.
+
+        Recuerdo con cariño las veces que nos hemos reunido en familia, y aunque no han sido muchas, cada momento ha sido especial. Su calidez y amabilidad siempre me han hecho sentir bienvenido. 
+
+        El ultimo reencuentro esta vez en su casa en Pisa, donde nos recibió, acogió, paseó y hasta nos preparo una de las comidas más sabrosas que he comido en mi vida nos hizo sentir como en casa, y allí confirmé que a pesar de la distancia y el tiempo que ha pasado desde la última vez que nos vimos. Fue un recordatorio de lo importante que es la familia, sin importar dónde estemos en el mundo.
+
+        Gracias por tanto cariño y atención que me brindó. Le deseo una Feliz navidad y le mando un enorme abrazo, esperando no falte mucho para ese próximo reencuentro🎄.
+
+        La quiero y extraño, que tenga una feliz navidad 🎄""",
+        "imagen_fondo": "https://images.unsplash.com/photo-1639949535040-f2b90ea4966a",
+        "imagen_carta": "https://i.postimg.cc/KYwJtNkH/Tia-ana.jpg",
+        "color_tema": "gold"
+    },
+    "eliecer_1963-09-22": {
+        "carta_id": "carta9",
+        "destinatario": "Tío Eliecer",
+        "fecha_nacimiento": "1963-09-22",
+        "titulo": "Querido Tío Eliecer",
+        "contenido": """Querido Tío Elicer,
+
+        Siempre lo recuerdo como un amante y apasionado por los carros, y la velocidad. últimanente me he vuelto más fan de la Formula 1 y me viene a la mente todas las veces que me ha contado anécdotas y experiencias relacionadas con el mundo del automovilismo. Su entusiasmo y conocimiento sobre el tema siempre han sido increible.
+
+        Recuerdo con mucha alegría esas vacaciones que coincidimos en Estados Unidos, era la primera vez que pisaba suelo gringo donde compartimos momentos inolvidables. Las risas, las aventuras y las historias que compartimos son recuerdos que nunca olvidaré.
+
+        Deseo que esta Navidad esté llena de alegría. Que la magia de esta temporada le traiga paz y felicidad, y que el próximo año esté colmado de bendiciones y salud.
+
+        Le mando un fuerte abrazo Tío Eliecer, Feliz Navidad🎅
+        """,
+        "imagen_fondo": "https://images.unsplash.com/photo-1638743264427-42b1399c0730",
+        "imagen_carta": "https://i.postimg.cc/tJ1CZ960/Tio-marco-eliecer.jpg",
+        "color_tema": "gold"
+    }
+}
+
+"""Eres el hermano, el hijo, el tío que todos admiramos. Tu capacidad para resolver cualquier problema y tu forma de hacer reír a todos, incluso en los momentos difíciles, es un regalo que valoramos más que cualquier cosa material.
 
 Esta época navideña, mientras reflexionamos sobre las bendiciones del año, tu nombre siempre está en primer lugar en nuestra lista de gratitud.
 
 Que el próximo año te traiga nuevas aventuras, mucha salud y que sigamos compartiendo muchas más Navidades juntos.
 
 Con profundo cariño,
-Tu familia que te valora enormemente 🎁""",
-        "imagen_fondo": "https://images.unsplash.com/photo-1602028393349-50cca57769e5",
-        "color_tema": "gold"
-    }
-}
+Tu familia que te valora enormemente 🎁"""
